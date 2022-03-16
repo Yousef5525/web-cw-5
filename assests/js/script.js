59a2f8dcac677cab557ee38ef297d142499fb7be
يوسف علاء عبده <joealaa552005@gmail.com>
2022-03-16 19:29:25 +0300

    day5 ex2

--- assests/js/script.js
+++ assests/js/script.js
@@ -3,4 +3,9 @@ alert(`يوسف علاء`);
 console.log(16);
 console.log(`انا في الصف الحادي عشر`);
 
+let buget = 20;
+console.log(buget >= 30 && buget <= 80);
+console.log(buget + 30);
+console.log(buget + 30 >= 30 && buget + 30 <= 80);
+
 // JavaScript is not Jave
