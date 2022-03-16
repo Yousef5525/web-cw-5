11de2b8aaf8114e457dc231a4d8159db26f0b8fb
يوسف علاء عبده <joealaa552005@gmail.com>
2022-03-16 20:28:40 +0300

    day5 ex3

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,5 +1,21 @@
 alert(`يوسف علاء`);
 
+let grade = prompt(`ادخل درجتك`);
+if (grade >= 90 && grade <= 100) {
+  console.log(`لقت حصلت على امتياز`);
+} else if (grade >= 80 && grade < 90) {
+  console.log(`لقد حصلت على جيد جدا`);
+} else if (grade >= 70 && grade < 80) {
+  console.log(`لقد حصلت على جيد`);
+} else if (grade >= 60 && grade < 70) {
+  console.log(`لقد حصلت على مقبول`);
+} else if (grade >= 50 && grade < 60) {
+  console.log(`لقد حصلت على ضعيف`);
+} else grade < 50;
+{
+  console.log(`راسب`);
+}
+
 console.log(16);
 console.log(`انا في الصف الحادي عشر`);
 
